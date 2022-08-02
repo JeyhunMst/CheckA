f090bac1012203f04cfb18a0df4745fbc125e812
JeyhunMst <109238733+JeyhunMst@users.noreply.github.com>
2022-08-03 02:16:39 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CheckA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the text:");
+            string text = Console.ReadLine();
+            bool check = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i]=='a')
+                {
+                    check = true;
+                    break;
+                }
+            }
+            if (check == true)
+            {
+                Console.WriteLine("There is a symbol in the text");
+
+            }
+            else 
+            {
+                Console.WriteLine("a symbol is not found in the text");
+
+            }
+                
+        }
+    }
+}
